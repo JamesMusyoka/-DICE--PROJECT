873eb5d75bb7ff14abd3acd2e47c1887e75ba656
James Musyoka <jamesmu475@gmail.com>
2018-12-17 09:35:49 +0300

    all of my folders and my files changes

--- js/scripts.js
+++ js/scripts.js
@@ -2,10 +2,16 @@ function rollDice() {
   var demo = document.getElementById("demo");
   demo.innerHTML = Math.floor(Math.random() * 6) +1;
 }
+var diceTotal = demo + mute;
+x.innerHTML = "You rolled" + diceTotal +".";
+if(demo == mute) {
+  x.innerHTML += "DOUBLES! You get a free turn!"
+}
 function rollDice() {
   var mute = document.getElementById("mute");
   mute.innerHTML = Math.floor(Math.random() * 6) +1;
 }
+
 var diceTotal = demo + mute;
 x.innerHTML = "You rolled" + diceTotal +".";
 if(demo == mute) {
